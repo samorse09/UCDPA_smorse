91b27f0c196596eb440247c74b58edae2583e001
samorse09 <80207980+samorse09@users.noreply.github.com>
2021-04-18 17:12:30 +0100

    Commit3

--- main.py
+++ main.py
@@ -20,20 +20,47 @@ header_data = df1.set_index('Unique ID').sort_index()
 print(header_data.head(2))
 hd_keys = header_data.keys()
 print(hd_keys)
+
 # Create subset to count number of individual customers
 hd_cusnum = header_data.drop_duplicates(subset='Customer Id')
 count_hd_cusnum = header_data.value_counts('Customer Id')
 print(count_hd_cusnum)
+
 #number of orders by customer
 cusords = header_data['Customer Id'].value_counts()
 print(cusords)
+
 #create subsets by late or on time
-dstatus = header_data['Late_delivery_risk'].value_counts()
-print(dstatus)
-#if header_data['Late_delivery_risk] == 0,
-ontime = header_data[header_data['Late_delivery_risk'] == 0]
-late = header_data[header_data['Late_delivery_risk'] == 1]
-print(np.shape(ontime))
-print(np.shape(late))
-#
+dstatus_num = header_data['Late_delivery_risk'].value_counts()
+print(dstatus_num)
+
+#add column for late status
+status =  []
+for value in header_data['Late_delivery_risk']:
+    if value == 1:
+        status.append("Late")
+    else:
+        status.append('On_Time')
+header_data['Late_Status'] = status
+print(header_data.head())
+
+is_late = header_data[header_data['Late_Status'] == 'Late']
+on_time = header_data[header_data['Late_Status'] == 'On_Time']
+
+print(np.shape(is_late))
+print(np.shape(on_time))
+
+lates_region = is_late['Order Region'].value_counts()
+print(lates_region)
+
+ontime_region = on_time['Order Region'].value_counts()
+print(ontime_region)
+
+total_byregion = header_data['Order Region'].value_counts()
+print(total_byregion)
+
+percent_byregion = lates_region / total_byregion
+print(percent_byregion)
+
+
 
