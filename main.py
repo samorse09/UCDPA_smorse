45eabbd8c117d92898cb1a4c7300d2ef413d5e22
samorse09 <80207980+samorse09@users.noreply.github.com>
2021-04-20 15:58:20 +0100

    Commit14 code cleaned up, descriptions added

--- main.py
+++ main.py
@@ -50,7 +50,7 @@ print(cusords)
 header_data['days_late'] = header_data['Days for shipping (real)'] - header_data['Days for shipment (scheduled)']
 print(header_data.head(5))
 
-#add column for late status value as this will be a major aspect of analysis & is a basis for major supply chain KPI
+#using the for function column for late status value as this will be a major aspect of analysis & is a basis for major supply chain KPI
 status =  []
 for value in header_data['Late_delivery_risk']:
     if value == 1:
@@ -75,9 +75,6 @@ print(order_data.head(5))
 all_data = header_data.merge(order_data, on='Unique ID')
 print(all_data.head(5))
 
-#check for any duplicates now that the dataframes are merged -
-
-
 #create sub groups for to separate out orders by late and on time for further analysis, print the shape to see number
 #of orders in each
 is_late = all_data[all_data['Late_Status'] == 'Late']
@@ -88,11 +85,14 @@ print(np.shape(on_time))
 #check the number of orders by customer type/segment to see if any major variances in qty there worth looking into
 orders_by_seg = all_data['Customer Segment'].value_counts
 
+
 #set two unique pallet lists for my plot
 palette1 = ['b','y','r']
 palette2 = ['c','m','b']
-
-#number of late orders vs. total number of orders by customer segment
+#####################
+##  FIRST PLOT ##
+####################
+#count plot - overlay number of late orders on top of total number of orders by customer segment
 orders_by_seg_plot = sns.countplot(x='Customer Segment', data=all_data,palette=palette1, saturation=.8).set_title('# Orders Late Out of Total')
 late_by_seg_plot = sns.countplot(x='Customer Segment',data=is_late,saturation=0.99,palette=palette2)
 plt.show()
@@ -102,53 +102,55 @@ total_bymarket = all_data['Market'].value_counts()
 percent_bymarket = lates_market / total_bymarket
 #print(percent_bymarket)
 
+#create a subgroup for looking at market related information for analysis - first check is the first and last year an
+#order was executed in each market
 market_alldata = all_data[['Market','Customer Id','YEAR OF ORDER','Late_Status','Department Name','Shipping Mode','Order Profit Per Order','Category Name']]
-#print(market_alldata.head(5))
-#market_alldata = all_data.set_index("Market")
-#min and max dates for each market
 market_group = market_alldata.groupby('Market')['YEAR OF ORDER'].agg([min,max])
 print(market_group)
 
-markets = all_data.groupby('Market')
+#discovered that only one market has any data in the year 2018, so going to drop all rows from year 2018 but first need
+#to rename the column
+all_data1 = all_data.rename(columns = {'YEAR OF ORDER' : 'Year'}, inplace = False)
+ad1_keys = all_data1.keys()
+print(ad1_keys)
+#remove all rows with year 2018
+fv_alldata = all_data1.drop(all_data1[all_data1.Year == 2018].index)
 
-africa = markets.get_group('Africa')
-asia = markets.get_group('Pacific Asia')
-latam = markets.get_group('LATAM')
-europe = markets.get_group('Europe')
-usca = markets.get_group('USCA')
+#create subgroups of the data by market
+markets = fv_alldata.groupby('Market')
+#africa = markets.get_group('Africa')
+#asia = markets.get_group('Pacific Asia')
+#latam = markets.get_group('LATAM')
+#europe = markets.get_group('Europe')
+#usca = markets.get_group('USCA')
 
-
-print(lates_market)
-print(percent_bymarket)
+#check the spread of orders by department within each market
 markets_dept = markets['Department Name'].value_counts()
 print(markets_dept)
 
+#total profit per year in each market
+market_profit = fv_alldata.groupby(['Market', 'Year'])['Order Profit Per Order'].agg(['mean','sum'])
+#name columns and check by printing
+market_profit.columns = ['Year Avg','Year Total']
+print(market_profit)
 
-pt1 = all_data.pivot_table(values='Order Profit Per Order',index='Market',columns='YEAR OF ORDER',aggfunc=np.sum, fill_value=0)
-print(pt1)
-
-#sns.countplot(x='Department Name',data=all_data)
-#plt.show()
-
-#count of orders by department in each market
-#sns.countplot(x='Department Name',data=all_data,hue='Market')
-#plt.show()
-
-#average profit per order based on department in each market
-#sns.catplot(x='Market',y='Order Profit Per Order',data=all_data,kind='bar',hue='Department Name',ci=none)
-#plt.show()
-
-#sns.catplot(x='Department Name',y='Order Profit Per Order',data=all_data,kind='bar',ci=none)
-#plt.show()
-
-#mall_date = market_alldata.sort_values('YEAR OF ORDER')
-#plot_a = sns.catplot(x='Department Name',data=mall_date,kind='count',hue='YEAR OF ORDER',col='Market', col_wrap=3)
-#plt.xticks(rotation=45)
-#plt.tight_layout()
-#plt.show()
-
-plot_b = sns.catplot(x='Shipping Mode',data=all_data,kind='count',hue='Late_Status')
+sns.countplot(x='Department Name',data=fv_alldata)
+plt.xticks(rotation=45)
 plt.show()
 
-plot_mode_region = sns.catplot(x='Market', kind='count',col='Shipping Mode', col_wrap=2, hue='Late_Status',data=all_data).set_xticklabels()
+#count of orders by department in each market
+sns.countplot(x='Department Name',data=fv_alldata,hue='Market')
+plt.xticks(rotation=45)
+plt.show()
+
+#average profit per order based on department in each market
+sns.catplot(x='Department Name',y='Order Profit Per Order',data=fv_alldata,kind='bar', ci=None)
+plt.xticks(rotation=45)
+plt.show()
+
+plot_b = sns.catplot(x='Shipping Mode',data=fv_alldata,kind='count',hue='Late_Status')
+plt.show()
+
+plot_mode_region = sns.catplot(x='Market', kind='count',col='Shipping Mode', col_wrap=2, hue='Late_Status',data=fv_alldata).set_xticklabels()
+plt.xticks(rotation=45)
 plt.show()
